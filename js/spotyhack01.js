bdf94b84073acaae3740c13bdd1c83e0a232ee87
EduRivera <eduardriveramestre@gmail.com>
2014-07-11 15:11:13 +0200

    added title,author, image

--- js/spotyhack01.js
+++ js/spotyhack01.js
@@ -2,19 +2,36 @@
 
 	'use strict';
 
-	var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'https://api.spotify.com/v1/tracks/7Bxv0WL7UC6WwQpk9TzdMJ');
+  
   var response;
 	xhr.onload = function () {
     if (this.status === 200) { // the result is OK
       response = JSON.parse(this.response);
       console.log('onload response', response);
+
     }
+    loadcontent();
+     
   };
-  var widget=document.querySelector('.widget');
-  //var audio=widget.getElementById('audio');
-  var title=widget.querySelector('.title');
-  title.contentText=response.name;
+
+  var loadcontent = function(){
+
+    var widget = document.querySelector('.widget');
+    //var audio=widget.getElementById('audio');
+    var title = widget.getElementsByClassName('title')[0];
+    var author = widget.getElementsByClassName('author')[0];
+    var image = widget.querySelector('#img');
+
+
+    title.textContent = response.name;
+    author.textContent = response.artists[0].name;
+    image.src = response.album.images[0].url;
+  }
+  
+  
+
   // send the request
   xhr.send();
 
