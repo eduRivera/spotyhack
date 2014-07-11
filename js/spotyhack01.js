f47a12d188547da8e6ff142c95877466afb97b3d
benja83 <tarennebenjamin@gmail.com>
2014-07-11 13:35:45 +0000

    added event play on click

--- js/spotyhack01.js
+++ js/spotyhack01.js
@@ -19,18 +19,25 @@
   var loadcontent = function(){
 
     var widget = document.querySelector('.widget');
-    //var audio=widget.getElementById('audio');
+    var audio = widget.querySelector('#audio');
     var title = widget.getElementsByClassName('title')[0];
     var author = widget.getElementsByClassName('author')[0];
     var image = widget.querySelector('#img');
+    var btnPlay = widget.querySelector('.btn-play');
 
-
+    /*step 1 change title and artist and image*/
     title.textContent = response.name;
     author.textContent = response.artists[0].name;
     image.src = response.album.images[0].url;
+
+    /*step 2 load mp3 in <audio>*/
+    audio.src=response.preview_url;
+    btnPlay.addEventListener('click', function (evt){
+      audio.play();
+    });
+
   }
-  
-  
+
 
   // send the request
   xhr.send();
