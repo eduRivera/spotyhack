a6c0b70bb4f07db97b924da67fece8ae211876f4
benja83 <tarennebenjamin@gmail.com>
2014-07-11 11:33:27 +0000

    mod js

--- js/spotyhack01.js
+++ js/spotyhack01.js
@@ -1,18 +1,21 @@
-(function (globa){
+(function (global){
 
 	'use strict';
 
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'https://api.spotify.com/v1/tracks/7Bxv0WL7UC6WwQpk9TzdMJ');
-
+  var response;
 	xhr.onload = function () {
-      if (this.status === 200) { // the result is OK
-        var response = JSON.parse(this.response);
-        console.log('onload response', response);
-      }
-    };
-
-    // send the request
-    xhr.send();
+    if (this.status === 200) { // the result is OK
+      response = JSON.parse(this.response);
+      console.log('onload response', response);
+    }
+  };
+  var widget=document.querySelector('.widget');
+  //var audio=widget.getElementById('audio');
+  var title=widget.querySelector('.title');
+  title.contentText=response.name;
+  // send the request
+  xhr.send();
 
 })(window);
